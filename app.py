734e6c9b9077c5f923afce293d5c8ad74b19d550
nabeel-313 <mominnabeelahmed@gmail.com>
2025-01-25 17:39:00 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from Chatbot.langchain_service.langchain_service import generate_response
 
-# user_message = 'Write a paragraph about life on Mars in year 2100.'
-# response = generate_response(user_message)
-# print(response)
 
 app = Flask(__name__)
 
